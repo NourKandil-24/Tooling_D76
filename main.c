f32a4100133d6794df22f54dd74ade80cdb87a1d
Ahmed Mohamed <nour_kandil@icloud.com>
2024-09-15 20:54:08 +0300

    v1.0.0

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Hamada 10 
+
+int main()
+{
+   int x = Hamada; 
+   int y = 45;
+   printf("X Value = %i\n", x); 
+   printf("Y Value = %i\n", y); 
+   return 0; 
+}
